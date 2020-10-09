db9123a71338576d92fc7b6e54389020738ee01d
Camilo Cunha de Azevedo <Camilotk@gmail.com>
2020-10-09 01:59:30 -0300

    updated the license headers of the files

--- src/app/resolvers/queries/index.js
+++ src/app/resolvers/queries/index.js
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) 2020 Fábrica de Sotware IFRS. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 let queries = {}
